ac8ea83880e851c5ebd4f762dde7cb7497891d39
Danny Paz <dan.pazuchowski@gmail.com>
2015-08-17 21:17:46 -0500

    added config and pipe callback for reader

--- src/routes/init.js
+++ src/routes/init.js
@@ -7,26 +7,34 @@ var parse = require('csv-parse');
 
 var readFile = function(path, callback){
   var response = [];
+  var skipFirstLine = true;
+  var parseOptions = {
+    delimiter: ',',
+    newline: '\n'
+  };
+  var parser = parse(parseOptions, function(err, data){
+    if (skipFirstLine === true){
+      skipFirstLine = false;
+    }else {
+      // Grab and then normalize
+      var ceoData = data[7] + data[6];
+      var ceoName = ceoData.replace('""', ' ');
 
-  var parser = parse({ delimiter: ',' }, function(err, data){
-    // Grab and then normalize
-    var ceoData = data[7] + data[6];
-    var ceoName = ceoData.replace('""', ' ');
+      var cuName = data[1].replace('"', ' ');
+      var cuZipcode = data[5].replace('"', "");
 
-    var cuName = data[1].replace('"', ' ');
-    var cuZipcode = data[5].replace('"', "");
-
-    response.push({
-      ceo_name: ceoName,
-      cu_name: cuName,
-      cu_zipcode: cuZipcode
-    });
+      response.push({
+        ceo_name: ceoName,
+        cu_name: cuName,
+        cu_zipcode: cuZipcode
+      });
+    }
   });
 
-  fs.createReadStream(filePath).pipe(parser, function(){
+  var reader = fs.createReadStream(filePath).pipe(parser);
+  reader.on('end', function(){
     callback(response);
   });
-
 };
 
 var initApi = function(req,res){
