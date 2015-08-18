4dced4dcbe9655bbff8be82b63da3957c88a2d4a
Danny Paz <dan.pazuchowski@gmail.com>
2015-08-17 23:36:07 -0500

    test file read and display

--- src/routes/init.js
+++ src/routes/init.js
@@ -4,39 +4,55 @@
 // in ./data/cu.txt to Mongo (or your configured database)
 var mongoose = require('mongoose');
 var fs = require('fs');
+var path = require('path');
 var parse = require('csv-parse');
 
-var readFile = function(path, callback){
+var readFile = function(filePath, callback){
   var response = [];
-  var skipFirstLine = true;
+  var firstLineExists = true;
   var parseOptions = {
     delimiter: ',',
     newline: '\n'
   };
 
   var parser = parse(parseOptions, function(err, data){
-    if (skipFirstLine === true){
-      skipFirstLine = false;
-    }else {
-      // Grab and then normalize
-      var ceoData = data[7] + data[6];
-      var ceoName = ceoData.replace('""', ' ');
+    if (data){
+      for(var i=0;i<data.length;i++){
+        if (firstLineExists){
+          firstLineExists = false;
+          continue;
+        }else {
+          console.log('Process the line');
+          // Grab and then normalize
+          var ceoData = data[i][6].split(",");
+          var ceoName = ceoData[1].trimLeft() + " " + ceoData[0];
 
-      var cuName = data[1].replace('"', ' ');
-      var cuZipcode = data[5].replace('"', "");
+          var cuName = data[i][1];
+          var cuZipcode = data[i][5];
 
-      response.push({
-        ceo_name: ceoName,
-        cu_name: cuName,
-        cu_zipcode: cuZipcode
-      });
+          response.push({
+            ceo_name: ceoName,
+            cu_name: cuName,
+            cu_zipcode: cuZipcode
+          });
+        }
+      }
     }
   });
 
-  var reader = fs.createReadStream(filePath).pipe(parser);
-  reader.on('end', function(){
-    callback(response);
-  });
+  try{
+    var reader = fs.createReadStream(path.join(__dirname, filePath)).pipe(parser);
+    reader.on('end', function(){
+      callback(false, response);
+    });
+    reader.on('error', function(){
+      // Need to find another way to handle this
+      callback(true);
+    });
+  } catch(err){
+    console.log(err);
+  }
+  
 };
 
 var initApi = function(req, res){
@@ -46,20 +62,27 @@ var initApi = function(req, res){
 
   var db = mongoose.connection.db;
   db.createCollection(collectionName, {strict: true}, function(err, collection){
-    if (err){
+    if (err === "banana"){
       // Just log if this errors
       // Non critical for init
       console.log(err);
       res('Data initialize already completed!');
     }else {
       readFile(filePath, function(err, rows){
-        var data = rows;
+        if (err) throw err;
+
+        var data = {
+          success: true,
+          filter: "none",
+          data: rows
+        };
+        res(data);
         // TODO
         // Add data
-        db.collection('credit_unions').insert(data, {}, function(err, records){
-          if (err) throw err;
-          res('Done!');
-        });
+        // db.collection('credit_unions').insert(data, {}, function(err, records){
+        //   if (err) throw err;
+        //   res('Done!');
+        // });
       });
     }
   });
